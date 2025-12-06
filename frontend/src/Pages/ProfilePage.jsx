245fdd268485a7bfffd7b7a4a779b1df8e085918
Shivraj Maharaul <shivrajmaharaul688@gmail.com>
2025-12-06 12:01:57 +0530

    Update ProfilePage to handle empty user data

--- frontend/src/Pages/ProfilePage.jsx
+++ frontend/src/Pages/ProfilePage.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom'
 const ProfilePage = () => {
     const navigate = useNavigate()
     const { user, logout } = useAuth()
+    
     return (
         <section className="min-h-screen bg-gray-800 flex items-center justify-center">
             <div className="h-96 w-80 flex flex-col items-center text-center shadow-sm shadow-gray-700 bg-gray-900 rounded-2xl justify-evenly">
@@ -13,8 +14,8 @@ const ProfilePage = () => {
                 </div>
                 <hr className="text-white w-72 mt-3" />
                 <div className="h-32 w-full px-4 flex flex-col items-start justify-around">
-                    <p className="text-white font-semibold text-md">Name : {user?.name || "Shivraj"}</p>
-                    <p className="text-white font-semibold text-md">Email : {user?.email || "shivrajmaharaul688@gmail.com"}</p>
+                    <p className="text-white font-semibold text-md">Name : {user?.name || ""}</p>
+                    <p className="text-white font-semibold text-md">Email : {user?.email || ""}</p>
                     <div className="flex items-center justify-evenly w-full">
                         <button className="bg-blue-400 py-2 text-sm px-2 text-white font-semibold rounded-lg w-28 hover:bg-blue-600 hover:cursor-pointer transition duration-200" onClick={() => navigate('/update-profile')}> Update Profile</button>
                         <button className="bg-transparent border border-red-400 py-2 text-sm px-2 font-semibold text-red-400 rounded-lg w-28 hover:bg-red-500 hover:text-white transition duration-200 cursor-pointer" onClick={() => logout()}>Logout</button>
