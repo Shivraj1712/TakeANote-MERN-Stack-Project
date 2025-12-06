932058ea812309f3e85e8bd3de17359dd2bee8a3
Shivraj Maharaul <shivrajmaharaul688@gmail.com>
2025-12-06 18:11:17 +0530

    Change Navbar link from Dashboard to Profile

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -105,7 +105,7 @@ export default function Navbar() {
                     {user ? (
                         <>
                             <Link
-                                to="/dashboard"
+                                to="/profile"
                                 onClick={() => setIsOpen(false)}
                                 className="hover:text-blue-300"
                             >
