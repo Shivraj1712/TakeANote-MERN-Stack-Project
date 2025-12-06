235df0351e0d897cd1710b2bc2e355737b35315a
Shivraj Maharaul <shivrajmaharaul688@gmail.com>
2025-12-06 13:29:03 +0530

    Refactor AuthContext for improved readability

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -4,43 +4,38 @@ import { useContext, useState, useEffect, createContext } from 'react'
 const AuthContext = createContext()
 
 export const AuthProvider = ({ children }) => {
-    // States of User and Loading 
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
 
-    // Fetch User
     const fetchUser = async () => {
         try {
             const { data } = await api.get('/user/profile')
             setUser(data)
         } catch (error) {
-            console.error(`${error.message}`)
+            console.error(error.message)
         } finally {
             setLoading(false)
         }
     }
-    // Calling the Fetch user function
+
     useEffect(() => {
         fetchUser()
-    }, [user])
-    // Login function 
-    const login = async ({ email, password }) => {
+    }, [])  
+
+    const login = async (email, password) => {
         const { data } = await api.post('/user/auth', { email, password })
         setUser(data)
     }
-    // Logout function
+
     const logout = async () => {
         try {
-            setLoading(true)
             await api.post('/user/logout')
-            setUser(null)
-        } catch (error) {
-            console.log("Backend offline, clearing user client-side")
         } finally {
-            setUser(null)
+            setUser(null)       
+            setLoading(false)
         }
     }
-    // Value that has to be passed on
+
     const value = {
         user,
         setUser,
@@ -50,11 +45,13 @@ export const AuthProvider = ({ children }) => {
         setLoading,
         isAuthenticated: !!user
     }
-    return <AuthContext.Provider value={value}>
-        {children}
-    </AuthContext.Provider>
+
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
 }
 
 const useAuth = () => useContext(AuthContext)
-
 export default useAuth
